9ed09af9ea4c10c6cc992e165f5916b2a9b47b1f
Paul Grey <paulgnz@users.noreply.github.com>
2023-01-11 14:49:18 +1300

    fix for vercel?

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,27 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
 //import adapter from '@sveltejs/adapter-node';
+import vercel from '@sveltejs/adapter-vercel';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		// default options are shown
+		adapter: vercel({
+		  // if true, will deploy the app using edge functions
+		  // (https://vercel.com/docs/concepts/functions/edge-functions)
+		  // rather than serverless functions
+		  edge: false,
+	
+		  // an array of dependencies that esbuild should treat
+		  // as external when bundling functions
+		  external: [],
+	
+		  // if true, will split your app into multiple functions
+		  // instead of creating a single one for the entire app
+		  split: false
+		})
 	},
 
 	preprocess: [
